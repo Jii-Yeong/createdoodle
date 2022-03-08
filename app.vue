a542a393a1d601bf4687e02d96104977477170a4
Ji Yeong Seo <tj5134@naver.com>
2022-03-09 01:48:52 +0900

    app.vue 작성

--- app.vue
+++ app.vue
@@ -1,2 +1,17 @@
 <template>
+  <navigation />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Navigation from '@src/component/navigation/Navigation.vue'
+import '@assets/css/main.scss'
+
+export default defineComponent({
+  components: { Navigation },
+  setup() {
+    Navigation
+  },
+})
+</script>
+
