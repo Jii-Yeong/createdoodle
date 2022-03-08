44c0c96fff859d4a059449982cc00ccf7b1f1997
Ji Yeong Seo <tj5134@naver.com>
2022-03-09 01:48:21 +0900

    카테고리 데이터 추가

---
+++ src/data/category.ts
@@ -0,0 +1,20 @@
+import { Category } from "../models/category";
+
+export const category: Category[] = [
+  {
+    upperTitle: '모후모후 게시판',
+    lowerList: [
+      '게시판1',
+      '게시판1',
+      '게시판1',
+    ]
+  },
+  {
+    upperTitle: '모후모후 게시판2',
+    lowerList: [
+      '게시판1',
+      '게시판1',
+      '게시판1',
+    ]
+  }
+]
