bbcb617616c146f52f7a5aff87d2d899bcda38a1
Ji Yeong Seo <tj5134@naver.com>
2022-03-11 23:22:08 +0900

    헤더 추가

---
+++ src/components/header/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header id="header">
+    <div>
+      <p class="title">끄적끄적</p>
+    </div>
+    <search-bar />
+  </header>
+</template>
+
+<style lang="scss" scoped>
+#header {
+  width: 100%;
+  height: 60px;
+  border-bottom: 1px solid #e2e2e2;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .title {
+    font-size: 18px;
+    margin: 13px;
+    padding: 0;
+    color: #5764ab;
+    font-weight: 700;
+  }
+}
+</style>
