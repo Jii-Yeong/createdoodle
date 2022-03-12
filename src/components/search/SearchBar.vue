733959c0536e4775c366364b955400460622a949
Ji Yeong Seo <tj5134@naver.com>
2022-03-12 20:45:31 +0900

    search bar 수정

--- src/components/search/SearchBar.vue
+++ src/components/search/SearchBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="search-bar">
-    <input type="text" class="search-input" />
+    <input type="text" class="search-input" placeholder="search..." />
     <button type="button" class="search-button">검색</button>
   </div>
 </template>
@@ -11,24 +11,25 @@
   align-items: center;
   margin: 0px 40px;
   .search-input {
-    width: 210px;
+    width: 170px;
     height: 37px;
     outline: none;
     border: none;
-    background-color: #c2c2c2;
     opacity: 0.5;
     border-radius: 20px 0px 0px 20px;
     padding: 0px 10px;
+    font-size: 17px;
+    font-weight: 500;
   }
   .search-button {
     outline: none;
-    border-radius: 0px 20px 20px 0px;
-    width: 80px;
-    height: 37px;
-    font-size: 16px;
+    border-radius: 4px;
+    width: 70px;
+    height: 30px;
+    font-size: 15px;
     border: none;
     background-color: #1f0d6d;
-    opacity: 0.7;
+    opacity: 0.5;
     color: #ffffff;
     cursor: pointer;
   }
