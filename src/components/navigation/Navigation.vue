9a69e8fb8bc17544f3876fe14ba5f673ada1427d
Ji Yeong Seo <tj5134@naver.com>
2022-03-12 20:44:54 +0900

    navigation 컴포넌트 생성

---
+++ src/components/navigation/Navigation.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav id="navigation">
+    <navigation-list
+      v-for="(list, index) in category"
+      :key="index"
+      :category="list"
+    />
+  </nav>
+</template>
+
+<script setup>
+import { category } from "@src/data/category";
+</script>
+
+<style lang="scss" scoped>
+#navigation {
+  width: 320px;
+  height: 93%;
+  position: relative;
+  border-right: 1px solid #c2c2c2;
+  position: fixed;
+  overflow-y: auto;
+}
+</style>
---
+++ src/components/navigation/NavigationList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="navigation-list">
+    <ul class="navigation-wrapper">
+      <li>{{ category.upperTitle }}</li>
+      <li
+        class="navigation-list"
+        v-for="(list, index) in category.lowerList"
+        :key="index"
+      >
+        {{ list }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Category } from "src/models/category";
+import { defineProps } from "vue";
+
+interface Props {
+  category: Category;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<style scoped lang="scss">
+#navigation-list {
+  margin: 35px 0px;
+  .navigation-wrapper {
+    list-style: none;
+    font-size: 17px;
+    li {
+      padding: 5px;
+      &:first-child {
+        font-size: 18px;
+        font-weight: 700;
+        color: #5764ab;
+      }
+      &:nth-child(n + 2) {
+        margin: 1px;
+      }
+    }
+  }
+}
+</style>
