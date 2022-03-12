5054d014344b97da95ede21ad3166705fcf5229a
Ji Yeong Seo <tj5134@naver.com>
2022-03-12 20:44:42 +0900

    헤더 배경 변경 및 마진 증가

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -15,9 +15,10 @@
   display: flex;
   justify-content: space-between;
   align-items: center;
+  background-color: #ffffff;
   .title {
     font-size: 18px;
-    margin: 13px;
+    margin: 13px 47px;
     padding: 0;
     color: #5764ab;
     font-weight: 700;
