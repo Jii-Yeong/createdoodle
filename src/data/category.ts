0f7d7c9bc8734ae5212ef514f666adb131a695b5
Ji Yeong Seo <tj5134@naver.com>
2022-03-12 20:45:15 +0900

    카테고리 목업 데이터 추가

--- src/data/category.ts
+++ src/data/category.ts
@@ -16,5 +16,45 @@ export const category: Category[] = [
       '게시판1',
       '게시판1',
     ]
-  }
+  },
+  {
+    upperTitle: '모후모후 게시판2',
+    lowerList: [
+      '게시판1',
+      '게시판1',
+      '게시판1',
+    ]
+  },
+  {
+    upperTitle: '모후모후 게시판2',
+    lowerList: [
+      '게시판1',
+      '게시판1',
+      '게시판1',
+    ]
+  },
+  {
+    upperTitle: '모후모후 게시판2',
+    lowerList: [
+      '게시판1',
+      '게시판1',
+      '게시판1',
+    ]
+  },
+  {
+    upperTitle: '모후모후 게시판2',
+    lowerList: [
+      '게시판1',
+      '게시판1',
+      '게시판1',
+    ]
+  },
+  {
+    upperTitle: '모후모후 게시판2',
+    lowerList: [
+      '게시판1',
+      '게시판1',
+      '게시판1',
+    ]
+  },
 ]
