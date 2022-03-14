d41bc8161b154661198c7a61d07fe8509816b584
Ji Yeong Seo <tj5134@naver.com>
2022-03-14 23:58:27 +0900

    MainPage.vue 와 app.vue 삭제

--- app.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <main-page />
-</template>
-
-<script setup lang="ts">
-import "@assets/css/main.scss";
-</script>
-
--- src/pages/main/MainPage.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <common-header />
-  <navigation />
-</template>
-
-<script setup lang="ts">
-import CommonHeader from "@src/components/header/Header.vue";
-</script>
