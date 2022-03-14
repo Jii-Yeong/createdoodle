f55ee4aeaf83026d93bf97e0324c565199c46c23
Ji Yeong Seo <tj5134@naver.com>
2022-03-15 00:01:05 +0900

    category data update

--- src/data/category.ts
+++ src/data/category.ts
@@ -2,59 +2,13 @@ import { Category } from "../models/category";
 
 export const category: Category[] = [
   {
-    upperTitle: '모후모후 게시판',
+    upperTitle: 'LANGUAGE',
     lowerList: [
-      '게시판1',
-      '게시판1',
-      '게시판1',
     ]
   },
   {
-    upperTitle: '모후모후 게시판2',
+    upperTitle: 'LIBRARY/FRAMEWORK',
     lowerList: [
-      '게시판1',
-      '게시판1',
-      '게시판1',
-    ]
-  },
-  {
-    upperTitle: '모후모후 게시판2',
-    lowerList: [
-      '게시판1',
-      '게시판1',
-      '게시판1',
-    ]
-  },
-  {
-    upperTitle: '모후모후 게시판2',
-    lowerList: [
-      '게시판1',
-      '게시판1',
-      '게시판1',
-    ]
-  },
-  {
-    upperTitle: '모후모후 게시판2',
-    lowerList: [
-      '게시판1',
-      '게시판1',
-      '게시판1',
-    ]
-  },
-  {
-    upperTitle: '모후모후 게시판2',
-    lowerList: [
-      '게시판1',
-      '게시판1',
-      '게시판1',
-    ]
-  },
-  {
-    upperTitle: '모후모후 게시판2',
-    lowerList: [
-      '게시판1',
-      '게시판1',
-      '게시판1',
     ]
   },
 ]
