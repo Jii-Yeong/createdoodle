c3e2d7e77a9756e0673f0f2749bb31d54f349870
Ji Yeong Seo <tj5134@naver.com>
2022-03-15 20:44:58 +0900

    fix .md path post component

--- src/components/post/Post.vue
+++ src/components/post/Post.vue
@@ -5,16 +5,14 @@
 </template>
 
 <script setup>
-import CommonHeader from "@src/components/header/Header.vue";
 import css from "@assets/css/main.scss";
-
 const route = useRoute();
 const id = route.params.id;
 const text = ref("");
 
 if (process.server) {
   const fs = await import("fs");
-  const content = fs.readFileSync(`posts/${id}.md`, "utf8");
+  const content = fs.readFileSync(`public/post/${id}.md`, "utf8");
   text.value = content;
 }
 </script>
