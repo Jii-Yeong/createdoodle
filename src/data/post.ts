b59969fb1f685a4ea117487d330888e2097b9276
Ji Yeong Seo <tj5134@naver.com>
2022-03-16 19:33:41 +0900

    add post list data

---
+++ src/data/post.ts
@@ -0,0 +1,10 @@
+import { PostList } from "src/models/post";
+
+export const postList: PostList[] = [
+  {
+    title: 'Nuxt3에서 마크다운 파일을 읽어 페이지에 띄우기',
+    description: 'fs 모듈을 이용하여 Nuxt3 페이지에 마크다운을 나타냅니다.',
+    thumbnail: '1',
+    route: 1,
+  }
+]
