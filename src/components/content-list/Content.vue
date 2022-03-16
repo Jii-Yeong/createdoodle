c9a2dd747ee3c754c3e893a2f20e3a3087c1fa4b
Ji Yeong Seo <tj5134@naver.com>
2022-03-16 19:34:14 +0900

    add content list component

---
+++ src/components/content-list/Content.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="content">
+    <div class="image-section">
+      <div ref="image"></div>
+    </div>
+    <nuxt-link
+      :to="{ name: 'post-id', params: { id: route } }"
+      class="post-information"
+    >
+      <p class="title">{{ title }}</p>
+      <p class="description">{{ description }}</p>
+    </nuxt-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, ref, Ref, onMounted } from "vue";
+
+const image: Ref = ref(null);
+const props = defineProps<Props>();
+
+onMounted(() => {
+  const imageElement = <HTMLImageElement>image.value;
+  imageElement.style.background = `center/contain no-repeat url('thumbnail/${props.thumbnail}.png')`;
+});
+
+interface Props {
+  title: string;
+  description: string;
+  thumbnail: string;
+  route: number;
+}
+</script>
+
+<style scoped lang="scss">
+.content {
+  width: 80%;
+  padding: 30px 20px;
+  margin: 30px 0px;
+  display: flex;
+  border: 1px solid #e2e2e2;
+  border-radius: 10px;
+  .image-section {
+    margin: 0px 30px;
+    width: 270px;
+    height: 170px;
+    background-color: #e2e2e2;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border-radius: 8px;
+    div {
+      width: 270px;
+      height: 170px;
+    }
+  }
+}
+
+.post-information {
+  margin: 0px 30px;
+  p {
+    margin: 0;
+    padding: 0;
+  }
+  .title {
+    color: #5764ab;
+    font-weight: 700;
+    font-size: 25px;
+    margin: 10px 0px;
+    word-break: keep-all;
+  }
+  .description {
+    color: #4e4e4e;
+  }
+}
+</style>
---
+++ src/components/content-list/ContentList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="content-list">
+    <content
+      v-for="(post, index) in postList"
+      :key="index"
+      :title="post.title"
+      :description="post.description"
+      :thumbnail="post.thumbnail"
+      :route="post.route"
+    />
+  </div>
+</template>
+
+<script setup>
+import { postList } from "@src/data/post";
+</script>
+
+<style scoped lang="scss">
+#content-list {
+  margin: 60px;
+  width: 75%;
+  height: 100%;
+  float: right;
+  padding: 50px 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
