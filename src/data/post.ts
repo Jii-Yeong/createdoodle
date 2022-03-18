24005314e97343d47300658b9e875e264ba617e9
Jii-Yeong <tj5134@naver.com>
2022-03-18 10:02:37 +0900

    add post list data

--- src/data/post.ts
+++ src/data/post.ts
@@ -6,5 +6,11 @@ export const postList: PostList[] = [
     description: 'fs 모듈을 이용하여 Nuxt3 페이지에 마크다운을 나타냅니다.',
     thumbnail: '1',
     route: 1,
-  }
+  },
+  {
+    title: 'vercel + nuxt3 전역 환경변수 설정하기',
+    description: 'vercel과 nuxt3에서 전역 환경변수를 설정합니다.',
+    thumbnail: '1',
+    route: 2,
+  },
 ]
