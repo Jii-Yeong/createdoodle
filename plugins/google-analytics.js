93f92248799f9e3397d67af8d26fd819b24d7066
Jii-Yeong <tj5134@naver.com>
2022-03-19 20:45:14 +0900

    Revert "add google-analytics plugin"
    This reverts commit 0deb6e4d143a19ea0c4e4e7f73bb725b5318f1d8.

--- plugins/google-analytics.js
+++
@@ -1,8 +0,0 @@
-import googleAnalytics from '@nuxtjs/google-analytics';
-
-export default defineNuxtPlugin((nuxtApp) => {
-  const googleAnalyticsId = useRuntimeConfig().googleAnalyticsId;
-  nuxtApp.vueApp.use(googleAnalytics, {
-    id: googleAnalyticsId
-  })
-})
