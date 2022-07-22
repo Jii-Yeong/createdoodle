01e7c99a2b2ea898967edace979855d6bdd07d6f
Jii-Yeong <tj5134@naver.com>
2022-07-22 20:34:06 +0900

    fix: 기존 해더 변경된 디자인 적용

--- src/components/header/CommonHeader.vue
+++ src/components/header/CommonHeader.vue
@@ -1,31 +1,37 @@
 <template>
   <header id="header">
-    <div>
-      <nuxt-link class="title" to="/">끄적끄적</nuxt-link>
+    <div class="header-content">
+      <mui-icon :size="60">arrow_drop_down</mui-icon>
+      <div>
+        <nuxt-link class="title" to="/">끄적끄적</nuxt-link>
+      </div>
     </div>
-    <!-- <search-bar /> -->
   </header>
 </template>
+
+<script setup lang="ts">
+import DeleteIcon from '@mui/icons-material/Delete'
+</script>
 
 <style lang="scss" scoped>
 #header {
   width: 100%;
   height: 60px;
-  border-bottom: 1px solid #e2e2e2;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #ffffff;
+  background-color: rgba(212, 205, 255, 0.29);
   position: fixed;
-  margin: 0;
-  z-index: 100;
-  .title {
-    font-size: 18px;
-    margin: 13px 47px;
-    padding: 0;
-    color: #5764ab;
-    font-weight: 700;
-    text-decoration: none;
+
+  .header-content {
+    margin-right: 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .title {
+      font-size: 25px;
+      padding: 0;
+      color: #ffffff;
+      font-weight: 700;
+      text-decoration: none;
+    }
   }
 }
 </style>
