bcce4b3faa051b7560691032eed38e2c9773817e
Jii-Yeong <tj5134@naver.com>
2022-07-22 20:33:03 +0900

    fix: upgrade nuxt3 version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt'
 import { resolve } from 'pathe'
 import Components from 'unplugin-vue-components/vite'
 import IconsResolver from 'unplugin-icons/resolver'
